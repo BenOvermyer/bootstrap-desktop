6f727071feffc3bf81f4272190df9beb76c78b78
Ben Overmyer <ben@benovermyer.com>
2018-06-15 19:03:45 +0000

    Add bash script for installing and triggering Ansible.

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y software-properties-common 
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+ansible-playbook desktop.yml
