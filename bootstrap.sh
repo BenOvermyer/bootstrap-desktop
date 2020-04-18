6e9437e84cbda4628bc63851b094c70696da80f8
Ben Overmyer <ben@overmyer.net>
2020-04-18 09:17:19 -0500

    Update in preparation for Pop!_OS 20.04

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [[ $# -eq 0 ]] ; then
-    echo 'This command requires a playbook name (e.g., "desktop").'
-    exit 0
+if [[ $# -eq 0 ]]; then
+  echo 'This command requires a playbook name (e.g., "desktop").'
+  exit 0
 fi
 
 playbook=$1
 
 sudo apt-get update
-sudo apt-get install -y software-properties-common 
+sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
